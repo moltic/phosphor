5c79991563ab47bd1ce47a49353cfa578c489f19
moltic <dbancalari@gmail.com>
2026-03-01 10:32:56 -0500

    fix: adjust dial overlay top calculation for improved positioning

--- ui/dials.js
+++ ui/dials.js
@@ -66,7 +66,7 @@ if (_editToggle) {
 export function updateDialOverlayTop() {
   const headerEl = document.getElementById('header');
   if (headerEl) {
-    _wrapEl.style.setProperty('--dial-overlay-top', (headerEl.offsetTop + headerEl.offsetHeight) + 'px');
+    _wrapEl.style.setProperty('--dial-overlay-top', (headerEl.offsetTop + headerEl.offsetHeight + 4) + 'px');
   }
 }
 
